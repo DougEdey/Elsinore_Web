16050a1812c29bd37f80fcaf6297b647229d652e
Doug Edey <dougedey@fastmail.com>
2021-02-27 19:37:27 +0800

    Add apollo client and list the available GraphQL probes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { ApolloProvider, useQuery, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://raspberrypi.local:8080/graphql',
+  cache: new InMemoryCache()
+})
+
+const PROBE_LIST = gql`
+  query {
+    probeList
+  }
+`;
+
+function UpdateProbeList() {
+  const { loading, error, data } = useQuery(PROBE_LIST);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p> Error :(</p>;
+
+  return data.probeList.map(address => {
+    return <div key={address}>
+      <p>
+        {address}: AVAILABLE
+      </p>
+    </div>
+  });
+}
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UpdateProbeList />
       </header>
     </div>
+    </ApolloProvider>
   );
 }
 

ada0d0a08014d9c4f3f571800d5c2d519ab02dd0
Doug Edey <dougedey@fastmail.com>
2021-06-06 11:41:33 +0800

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'eslint:recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'comma-dangle': 'always'
+  },
+  globals: {
+    React: 'writable'
+  }
+}
--- components/DeviceList.js
+++ components/DeviceList.js
@@ -1,6 +1,6 @@
-import { gql, useQuery, NetworkStatus } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import ErrorMessage from './ErrorMessage'
-// import PostUpvoter from './PostUpvoter'
+
 
 export const ALL_DEVICES_QUERY = gql`
   query deviceList {
@@ -14,34 +14,15 @@ export const ALL_DEVICES_QUERY = gql`
   }
 `
 
-// export const allPostsQueryVars = {
-//   skip: 0,
-//   first: 10,
-// }
-
-export default function DeviceList() {
+export default function DeviceList () {
   const { loading, error, data } = useQuery(
     ALL_DEVICES_QUERY,
     {
-      // variables: allPostsQueryVars,
-      // Setting this value to true will make the component rerender when
-      // the "networkStatus" changes, so we are able to know if it is fetching
-      // more data
       notifyOnNetworkStatusChange: true,
       pollInterval: 500,
       credentials: 'same-origin',
     }
   )
-
-  // const loadingMorePosts = networkStatus === NetworkStatus.fetchMore
-
-  // const loadMorePosts = () => {
-  //   fetchMore({
-  //     variables: {
-  //       skip: allPosts.length,
-  //     },
-  //   })
-  // }
 
   if (error) {
     return <ErrorMessage message="Error loading devices." />
@@ -63,45 +44,7 @@ export default function DeviceList() {
             })}
           </li>
         ))}
-      </ul>      
-      <style jsx>{`
-        section {
-          padding-bottom: 20px;
-        }
-        li {
-          display: block;
-          margin-bottom: 10px;
-        }
-        div {
-          align-items: center;
-          display: flex;
-        }
-        a {
-          font-size: 14px;
-          margin-right: 10px;
-          text-decoration: none;
-          padding-bottom: 0;
-          border: 0;
-        }
-        span {
-          font-size: 14px;
-          margin-right: 5px;
-        }
-        ul {
-          margin: 0;
-          padding: 0;
-        }
-        button:before {
-          align-self: center;
-          border-style: solid;
-          border-width: 6px 4px 0 4px;
-          border-color: #ffffff transparent transparent transparent;
-          content: '';
-          height: 0;
-          margin-right: 5px;
-          width: 0;
-        }
-      `}</style>
+      </ul>
     </section>
   )
 }

d18cef93bec3b6f4a66dbdbddd5078b65893a441
Doug Edey <dougedey@fastmail.com>
2021-06-06 11:45:51 +0800

    Add Eslint Workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:react/recommended',
     'standard',
-    'eslint:recommended'
+    'eslint:recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
     'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'comma-dangle': 'always'
+    'comma-dangle': ['error', 'always'],
   },
   globals: {
-    React: 'writable'
-  }
+    React: 'writable',
+  },
 }
